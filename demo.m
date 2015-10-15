15aa59b310b16b5834997f9ccb5049151329b006
Sam Toyer <sam@qxcv.net>
2015-10-15 23:33:22 +1100

    Now store2hdf5 actually works (!)

--- demo.m
+++ demo.m
@@ -16,13 +16,18 @@ parfor i=1:size(pairs, 1)
     cached_imflow(fst, snd, conf.cache_dir);
 end
 
-for i=size(pairs, 1)
+patch_dir = fullfile(conf.cache_dir, 'patches');
+if ~exist(patch_dir, 'dir')
+    mkdir(patch_dir);
+end
+    
+for i=1:size(pairs, 1)
     fprintf('Working on pair %d/%d\n', i, size(pairs, 1));
     fst = flic_data(pairs(i, 1));
     snd = flic_data(pairs(i, 2));
     
     stack_start = tic;
-    stacks = get_stack(...
+    stacks = get_stacks(...
         conf, fst, snd, conf.aug.flips, conf.aug.rots, conf.aug.scales, ...
         conf.aug.randtrans);
     stack_time = toc(stack_start);
@@ -33,12 +38,12 @@ for i=size(pairs, 1)
         % Get stack and labels; we don't add in dummy dimensions because
         % apparently Matlab can't tell the difference between a
         % j*k*l*1*1*1*1... matrix and a j*k*l matrix.
-        stack = stacks(i).stack;
-        labels = stacks(i).labels;
+        stack = stacks(j).stack;
+        labels = stacks(j).labels;
         
         % Choose a file, regardless of whether it exists
         h5_idx = randi(conf.num_hdf5s);
-        filename = h5_name(h5_idx);
+        filename = fullfile(patch_dir, sprintf('samples-%06i.h5', h5_idx));
         create = 0;
         if ~exist(filename, 'file')
             create = 1;
@@ -50,6 +55,3 @@ for i=size(pairs, 1)
     write_time = toc(write_start);
     fprintf('Writing %d examples took %fs\n', length(stacks), write_time);
 end
-
-function name = h5_name(idx)
-name = fullfile(conf.cache_dir, 'patches', sprintf('samples-%06i.h5', idx));
