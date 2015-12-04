9d8f442d43631c32354edf386219675f966d396b
Sam Toyer <sam@qxcv.net>
2015-12-04 17:12:58 +1100

    Start on biposelet stuff

---
+++ main_poselets.m
@@ -0,0 +1,3 @@
+% This serves a similar purpose to main_dual, except it runs experiments
+% related to Anoop's poselet + regression idea (no use of motion
+% information for now).
---
+++ train/cluster_poselets.m
@@ -0,0 +1,5 @@
+function centroids = cluster_poselets(poses, num_classes)
+%CLUSTER_POSELETS Get poselet class centroids using K-means
+
+end
+
