e71a76d6fb6a6e4b8cf7de67043a01b515f7eb32
Sam Toyer <sam@qxcv.net>
2015-12-11 00:52:43 +1100

    I unreservedly blame Matlab

--- train/get_stacks.m
+++ train/get_stacks.m
@@ -45,9 +45,9 @@ for flip=flips
         % FIXME: This code will break if I use a data set other than
         % FLIC
         num_joints = size(d1.joint_locs, 1);
-        asset(num_joints == size(d2.joint_locs, 1));
-        flip_joints(1:num_joints) = flip_lr(flip_joints(1:num_joints));
-        flip_joints(num_joints+1:end) = flip_lr(flip_joints(num_joints+1:end));
+        assert(num_joints == size(d2.joint_locs, 1));
+        flip_joints(1:num_joints, :) = flip_lr(flip_joints(1:num_joints, :));
+        flip_joints(num_joints+1:end, :) = flip_lr(flip_joints(num_joints+1:end, :));
         % Reverse images
         flip_stack = flip_stack(:, end:-1:1, :);
         % Flip flow
@@ -143,6 +143,6 @@ end
 
 function flipped = flip_lr(joints)
 % XXX: This will break for datasets other than FLIC
-flipped = joints([4:6 1:3 10 8:9 7 11:12 14 13 15:29]);
-assert(all(size(flipped) == size(jonits)));
+flipped = joints([4:6 1:3 10 8:9 7 11:12 14 13 15:29], :);
+assert(all(size(flipped) == size(joints)));
 end
