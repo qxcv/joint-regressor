316279573db27456ed96dae87867e19743099fe8
Sam Toyer <sam@qxcv.net>
2016-03-07 11:00:41 +1100

    Get CNN eval working

--- keras/cnn_eval.m
+++ keras/cnn_eval.m
@@ -1,13 +1,30 @@
-function [left_locs, right_locs, head_locs, classes] = cnn_eval(model, images_data, flow_data)
+function [left_locs, right_locs, head_locs, classes] = cnn_eval(model, images_data, flow_data, mean_pixels)
 %CNN_EVAL Evaluates our CNN by calling into Python
 % Our code uses Keras, so evaluating directly in Matlab (or a mex library)
 % won't work here.
+assert(ndims(images_data) == 4, 'Need 4D RGB tensor (N6HW)');
+assert(ndims(flow_data) == 4, 'Need 4D flow tensor (N2HW)');
+assert(size(images_data, 2) == 6, 'RGBRGB channels should be second axis');
+assert(size(flow_data, 2) == 2, 'uv channels should be second axis');
+assert(isvector(mean_pixels.images) && isvector(mean_pixels.flow));
+
+% Format for Keras input data: N*C*H*W (see
+% http://keras.io/layers/convolutional/#convolution2d). I've been feeding
+% it N*C*S*S, so hopefully the first S is height and the second is width :)
+im_mean_pixel = reshape(mean_pixels.images, [1 length(mean_pixels.images) 1 1]);
+norm_images_data = bsxfun(@minus, double(images_data), im_mean_pixel);
+flow_mean_pixel = reshape(mean_pixels.flow, [1 length(mean_pixels.flow) 1 1]);
+norm_flow_data = bsxfun(@minus, double(flow_data), flow_mean_pixel);
+
+% Build dict and evaluate
 data_dict = py.dict();
-data_dict{'images'} = images_data;
-data_dict{'flow'} = flow_data;
+data_dict.setdefault('images', matlab2numpy(norm_images_data));
+data_dict.setdefault('flow', matlab2numpy(norm_flow_data));
 results = model.predict(data_dict);
-left_locs = cnn_numpy_to_matlab(results{'left'});
-right_locs = cnn_numpy_to_matlab(results{'right'});
-head_locs = cnn_numpy_to_matlab(results{'head'});
-classes = cnn_numpy_to_matlab(results{'class'});
+
+% Get results
+left_locs = numpy2matlab(results.get('left'));
+right_locs = numpy2matlab(results.get('right'));
+head_locs = numpy2matlab(results.get('head'));
+classes = numpy2matlab(results.get('class'));
 end
--- keras/matlab2numpy.m
+++ keras/matlab2numpy.m
@@ -5,6 +5,6 @@ assert(isnumeric(some_data));
 np = cnn_get_module('numpy');
 % Only using int32 because array.array doesn't support int64
 real_shape = py.tuple(int32(size(some_data)));
-np_data_unshaped = np.array(some_data(:)');
+np_data_unshaped = np.array.feval(some_data(:)');
 np_data = np_data_unshaped.reshape(real_shape, pyargs('order', 'F'));
 end
