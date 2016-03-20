17de78a46889d6fa549a0367547f2520c3242165
Sam Toyer <sam@qxcv.net>
2016-03-20 14:14:57 +1100

    Code to scale subposes properly

---
+++ train/norm_subpose.m
@@ -0,0 +1,19 @@
+function normed_locs = norm_subpose(d1, d2, scale, cnn_window, subpose_idxs)
+%NORM_SUBPOSE Norm joint coords to lie in CNN window and be well scaled
+assert(isscalar(scale));
+assert(isvector(cnn_window));
+assert(isvector(subpose_idxs) && isnumeric(subpose_idxs));
+
+joints = cat(2, d1.joint_locs(subpose_idxs, :), d2.joint_locs(subpose_idxs, :));
+assert(ismatrix(joints) && ~isvector(joints));
+maxes = max(joints, [], 1);
+mins = min(joints, [], 1);
+midpoint = mins + (maxes - mins) ./ 2;
+anchor = midpoint - scale;
+trans_joint_locs = bsxfun(@minus, joints, reshape(anchor, [1 2]));
+scale_factors = cnn_window ./ scale;
+normed_locs = bsxfun(@times, trans_joint_locs, reshape(scale_factors, [1 2]));
+assert(all(normed_locs >= 0));
+assert(all(bsxfun(@lt, normed_locs, reshape(cnn_window, [1 2]))));
+assert(all(size(normed_locs) == size(joints)));
+end
