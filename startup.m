1f39c4bd5933626f419e334b20bd5184a439af3b
Sam Toyer <sam@qxcv.net>
2016-04-21 16:33:37 +1000

    More useful startup script

--- startup.m
+++ startup.m
@@ -1,6 +1,18 @@
 % General startup code
 
+if isempty(gcp('nocreate')) && isunix
+    [stat, res] = system('free -g | sed -e ''s/\s\+/ /g'' | grep ^Mem: | cut -f 2 -d " "');
+    if ~stat && str2double(res) > 32
+        % If we have memory to burn, then we can make a pool that lasts a
+        % few days.
+        fprintf('Starting long-lived pool\n');
+        parpool('IdleTimeout', 48 * 60);
+    end
+    clear stat res;
+end
+
 addpath_full ./;
+addpath_full ./eval;
 addpath_full ./datasets/;
 addpath_full ./detect/;
 addpath_full ./util/;
@@ -11,15 +23,17 @@ addpath_full ./keras/;
 addpath_full ./cy/;
 
 % Chen & Yuille code
+
 old = cd('./cy');
+cd_reset = onCleanup(@() cd(old));
 CY_startup;
 CY_compile;
-cd(old);
+cd_reset.task();
 
 % Cherian et al. code
-old = cd('./cmas');
+cd('./cmas');
 CMAS_startup;
-cd(old);
+cd_reset.task();
 
 conf = get_conf();
 if ~exist(conf.cache_dir, 'dir')
@@ -28,3 +42,5 @@ if ~exist(conf.cache_dir, 'dir')
 end
 addpath_full(conf.ext_dir);
 get_deps;
+
+clear old cd_reset conf;
