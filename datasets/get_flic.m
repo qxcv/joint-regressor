67566cece0ed19c85a5c6207cb2a7d72eba71c54
Sam Toyer <sam@qxcv.net>
2016-05-10 14:03:40 +1000

    Ensure consistent choice of subjects for pairs

--- datasets/get_flic.m
+++ datasets/get_flic.m
@@ -21,6 +21,22 @@
 % 17 Nose
 % 19-29 N/A
 % We probably only want to return a subset of those
+%
+% Note that there are two potentially handy attributes in FLIC for avoiding
+% crappy poses:
+%
+% 1) .isbad tells you when a pose is hard to estimate (occlusion,
+%    non-frontal people, several people in the same frame, etc.). It
+%    doesn't take much to get flagged by .isbad, which hurts its utility in
+%    our application.
+% 2) .isunchecked seems to be set iff the pose wasn't checked by a
+%    researcher after being labelled by a turker. Some of those are
+%    probably really terrible (e.g. I've seen poses with totally
+%    nonsensical labels which probably had that flag set).
+%
+% Since neither of those flags do exactly what I want (find fully occluded
+% joints), I'll probably have to go through the pairs my code finds
+% manually and correct them.
 
 function [train_dataset, val_dataset] = get_flic(dest_dir, cache_dir, ...
     subposes, step, template_scale, trans_spec)
@@ -30,7 +46,12 @@ CACHE_PATH = fullfile(cache_dir, 'FLIC-full.zip');
 % If two samples are within 20 frames of each other, then they can be used
 % for training. Some frames are too far apart to reliably compute flow, so
 % we ignore them.
-FRAME_THRESHOLD = 20;
+FRAME_THRESHOLD = 15;
+% If the mean distance between joints for potentially neighbouring frames
+% is greater than this amount, then we don't consider them neighbours. This
+% avoids situations where one frame is included twice with different people
+% labelled each time.
+L2_THRESHOLD = 50;
 
 % Just in case :)
 mkdir_p(cache_dir);
@@ -49,7 +70,8 @@ flic_examples = flic_examples_s.examples;
 empty = cell([1 length(flic_examples)]);
 flic_data = struct('frame_no', empty, 'movie_name', empty, ...
     'image_path', empty, 'orig_joint_locs', empty, 'joint_locs', empty, ...
-    'torso_box', empty, 'is_train', empty, 'is_test', empty);
+    'torso_box', empty, 'is_train', empty, 'is_test', empty, ...
+    'is_unchecked', empty, 'is_hard', empty);
 parfor i=1:length(flic_examples)
     ex = flic_examples(i);
     flic_data(i).frame_no = ex.currframe;
@@ -62,6 +84,8 @@ parfor i=1:length(flic_examples)
     flic_data(i).torso_box = ex.torsobox;
     flic_data(i).is_train = ex.istrain;
     flic_data(i).is_test = ex.istest;
+    flic_data(i).is_unchecked = ex.isunchecked;
+    flic_data(i).is_hard = ex.isbad;
 end
 
 % Find close pairs of frames from within the FLIC+ indices. The .istrain
@@ -78,11 +102,11 @@ for i=1:length(test_movies)
 end
 
 val_inds = find(val_mask);
-val_pairs = find_pairs(val_inds, flic_data, FRAME_THRESHOLD);
+val_pairs = find_pairs(val_inds, flic_data, FRAME_THRESHOLD, L2_THRESHOLD);
 val_dataset = unify_dataset(flic_data, val_pairs, 'val_dataset_flic');
 
 train_inds = find(~val_mask);
-train_pairs = find_pairs(train_inds, flic_data, FRAME_THRESHOLD);
+train_pairs = find_pairs(train_inds, flic_data, FRAME_THRESHOLD, L2_THRESHOLD);
 train_dataset = unify_dataset(flic_data, train_pairs, 'train_dataset_flic');
 
 [train_dataset, ~] = mark_scales(train_dataset, subposes, step, ...
@@ -91,13 +115,35 @@ train_dataset = unify_dataset(flic_data, train_pairs, 'train_dataset_flic');
     template_scale, [train_dataset.pairs.scale]);
 end
 
-function pairs = find_pairs(inds, flic_data, thresh)
-fst_inds = inds(1:end-1);
-snd_inds = inds(2:end);
-names_eq = strcmp({flic_data(fst_inds).movie_name}, {flic_data(snd_inds).movie_name});
-fdiffs = [flic_data(snd_inds).frame_no] - [flic_data(fst_inds).frame_no];
-firsts = find(names_eq & fdiffs > 0 & fdiffs <= thresh);
-pairs = cat(2, firsts', firsts'+1);
+function pairs = find_pairs(inds, flic_data, frame_skip_thresh, ...
+    pose_mean_l2_thresh)
+pairs = zeros([0 2]);
+lookahead = 10;
+
+for ind_idx=1:length(inds)-1
+    % For each index into a frame we care about, we'll look ahead up to 10
+    % indices (arbitrary) to find a frame which (a) has the same movie name
+    % (b) has a frame number within thresh of the current frame number and
+    % (c) has joint labels which actually make sense.
+    ind = inds(ind_idx);
+    eligible_inds = inds(ind_idx+1:min(ind_idx+lookahead, end));
+    eligible_data = flic_data(eligible_inds);
+    names_valid = strcmp(flic_data(ind).movie_name, ...
+        {eligible_data.movie_name});
+    frame_diffs = [eligible_data.frame_no] ...
+        - flic_data(ind).frame_no;
+    frames_valid = frame_diffs > 0 & frame_diffs <= frame_skip_thresh;
+    these_joints = flic_data(ind).joint_locs;
+    all_mean_l2s = cellfun(@(p) mean_dists(these_joints, p), ...
+        {eligible_data.joint_locs});
+    dists_valid = all_mean_l2s < pose_mean_l2_thresh;
+    
+    okay_idxs = eligible_inds(names_valid & frames_valid & dists_valid);
+    
+    if ~isempty(okay_idxs)
+        pairs = [pairs; ind okay_idxs(1)]; %#ok<AGROW>
+    end
+end
 end
 
 function locs = convert_joints(orig_locs)
