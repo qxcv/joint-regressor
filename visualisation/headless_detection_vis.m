de679f64a9e642678fab2103e677c4478b16e8c7
Sam Toyer <sam@qxcv.net>
2016-05-17 11:24:08 +1000

    Visualising detection sequences, too

---
+++ visualisation/headless_detection_vis.m
@@ -0,0 +1,43 @@
+function headless_detection_vis(dataset, pose_dets, dest_dir)
+%HEADLESS_DS_VIS Visualise detections like headless_ds_vis does for GTs.
+% Nice for making pretty movies
+figure('Visible', 'off');
+axes('Visible', 'off');
+set(0, 'DefaulttextInterpreter', 'none')
+mkdir_p(dest_dir);
+all_data = dataset.data;
+seqs = dataset.seqs;
+
+parfor seq_idx=1:length(dataset.seqs)
+    seq = seqs{seq_idx};
+    
+    fprintf('Working on seq %i/%i\n', seq_idx, length(seqs)); %#ok<PFBNS>
+    for frame_idx=1:length(seq)
+        datum = all_data(seq(frame_idx)); %#ok<PFBNS>
+        pred_joints = pose_dets{seq_idx}{frame_idx};
+        
+        im = readim(datum);
+        imagesc(im);
+        axis image off;
+        axis equal;
+        
+        hold on;
+        plot_joints(pred_joints);
+        hold off;
+        
+        %label = get_label(datum, seq(frame_idx));
+        %text(-50, -50, label, 'Interpreter', 'none');
+        result_path = fullfile(dest_dir, ...
+            sprintf('seq-%03i-frame-%04i.jpg', seq_idx, frame_idx));
+        print(gcf, '-djpeg', result_path, '-r 300');
+    end
+end
+end
+
+function label = get_label(datum, idx)
+if hasfield(datum, 'image_path')
+    label = datum.image_path;
+else
+    label = sprintf('Index %i', idx);
+end
+end
