4738b212bba4934e1d94cb7aadb64c6257a7db19
Sam Toyer <sam@qxcv.net>
2016-05-23 09:51:31 +1000

    Last semester

--- paper/scripts/presentation_plot.sh
+++ paper/scripts/presentation_plot.sh
@@ -3,6 +3,8 @@
 ./plot_pck.py \
     --input "Cherian et al." ../stats/mpii/cmas/pck-mpii-cmas.csv \
     --input "$(printf 'Pfister et al.\n(SpatialNet)')" ../stats/mpii/pcz/pck-mpii-pcz.csv \
+    `# this is a really cool abuse of bacticks :D` \
+    `# --input "Last semester" ../stats/mpii/comp2560/pck-mpii-comp2560.csv` \
     --input "Biposelets" ../stats/mpii/mine/2016-05-17-pck-mpii-mine.csv \
     --poster --dims 10 4 \
     $@
