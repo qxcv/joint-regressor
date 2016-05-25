52230200cb0e81c0a9343a5945757d9703b7bc54
Sam Toyer <sam@qxcv.net>
2016-05-25 21:49:58 +1000

    Make style work

---
+++ paper/scripts/report_plot_mpii.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+./plot_pck.py \
+    --input 'Cherian et al.~\cite{cherian2014mixing}' ../stats/mpii/cmas/pcks.csv \
+    --input 'Chen \& Yuille~\cite{chen2014articulated}' ../stats/mpii/cy/pcks.csv \
+    --input 'Pfister et al.\\(SpatialNet)~\cite{pfister2015flowing}' ../stats/mpii/pcz/pcks.csv \
+    --dims 4.8 1.6 \
+    --xmax 30 \
+    --save ../figures/plots/mpii-pck.pgf
+    $@
