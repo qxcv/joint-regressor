8c6ed940aaba32a8030c16928bad989cb00f0f3b
Sam Toyer <sam@qxcv.net>
2016-05-27 13:59:04 +1000

    Some suggestions from Anoop

--- paper/scripts/report_plot_mpii.sh
+++ paper/scripts/report_plot_mpii.sh
@@ -4,7 +4,6 @@
     --input 'Cherian et al.~\cite{cherian2014mixing}' ../stats/mpii/cmas/pcks.csv \
     --input 'Chen \& Yuille~\cite{chen2014articulated}' ../stats/mpii/cy/pcks.csv \
     --input 'Combined~\cite{cherian2014mixing} and~\cite{chen2014articulated}' ../stats/mpii/comp2560/pcks.csv \
-    --input 'Pfister et al. (SpatialNet)~\cite{pfister2015flowing}' ../stats/mpii/pcz/pcks.csv \
     --input 'Biposelets' ../stats/mpii/biposelets/pcks.csv \
     --dims 4.8 2 \
     --xmax 30 \
