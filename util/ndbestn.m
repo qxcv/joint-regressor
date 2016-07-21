7c18c352e0a7e691b9e74745d7579409437a1c88
Sam Toyer <sam@qxcv.net>
2016-07-21 22:54:45 +1000

    Minor fix to ndbestn

--- util/ndbestn.m
+++ util/ndbestn.m
@@ -1,6 +1,6 @@
 function [varargout] = ndbestn(in_mat, N)
 %NDBESTN Return ND subscripts of maximum N results
-assert(nargout == ndims(in_mat), 'Need an output for each dimension');
+assert(nargout >= ndims(in_mat), 'Need an output for each dimension');
 assert(numel(in_mat) >= N, 'Need at least N inputs to produce N outputs');
 varargout = cell([1 nargout]);
 [~, sorted_inds] = sort(in_mat(:), 'Descend');
