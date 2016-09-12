fb2f249302549b1e0c180a699f5fd1c16917f359
Sam Toyer <sam@qxcv.net>
2016-09-12 11:25:19 +1000

    Record timings in diary

--- main_generic.m
+++ main_generic.m
@@ -14,75 +14,107 @@ fprintf('Command: %s\n', gpu_run_cmd);
 gcp;
 pctRunOnAll(gpu_run_cmd);
 
+valWriteStart = tic;
 fprintf('Writing validation set\n');
 val_patch_dir = fullfile(conf.cache_dir, 'val-patches');
 write_dset(val_dataset, val_patch_dir, conf.num_val_hdf5s, ...
     conf.cnn.window, conf.cnn.step, conf.subposes, conf.left_parts, ...
     conf.right_parts, conf.val_aug, conf.val_chunksz);
+fprintf('[T] Getting validation set took %fs\n', toc(valWriteStart));
 
+trainWriteStart = tic;
 fprintf('Writing training set\n');
 train_patch_dir = fullfile(conf.cache_dir, 'train-patches');
 write_dset(train_dataset, train_patch_dir, conf.num_hdf5s, ...
     conf.cnn.window, conf.cnn.step, conf.subposes, conf.left_parts, ...
     conf.right_parts, conf.aug, conf.train_chunksz);
+fprintf('[T] Getting training set took %fs\n', toc(trainWriteStart));
 
+clusterStart = tic;
 fprintf('Writing cluster information\n');
 biposelets = cluster_h5s(conf.biposelet_classes, conf.subposes, ...
     train_patch_dir, val_patch_dir, conf.cache_dir);
+fprintf('[T] Writing clusters took %fs\n', toc(clusterStart));
 
+trainNegWriteStart = tic;
 fprintf('Writing training negatives (from training set)\n');
 write_negatives(train_dataset, biposelets, train_patch_dir, ...
     conf.cnn.window, conf.aug, conf.train_chunksz, conf.subposes, ...
     'negatives.h5');
+fprintf('[T] Writing training negatives took %fs\n', toc(trainNegWriteStart));
+
+inriaWriteStart = tic;
 fprintf('Writing training negatives (from INRIA)\n');
 write_negatives(neg_dataset, biposelets, train_patch_dir, ...
     conf.cnn.window, conf.aug, conf.train_chunksz, conf.subposes, ...
     'inria-negatives.h5');
+fprintf('[T] Writing INRIA training negatives took %fs\n', toc(inriaWriteStart));
 
+valWriteNegStart = tic;
 fprintf('Writing validation negatives (from validation set)\n');
 write_negatives(val_dataset, biposelets, val_patch_dir, ...
     conf.cnn.window, conf.val_aug, conf.val_chunksz, conf.subposes, ...
     'negatives.h5');
+fprintf('[T] Writing validation negatives took %fs\n', toc(valWriteNegStart));
+
+valInriaWriteStart = tic;
 fprintf('Writing validation negatives (from INRIA)\n');
 write_negatives(neg_dataset, biposelets, val_patch_dir, ...
     conf.cnn.window, conf.val_aug, conf.val_chunksz, conf.subposes, ...
     'inria-negatives.h5');
+fprintf('[T] Writing INRIA validation negatives took %fs\n', toc(valInriaWriteStart));
 
 % We need to re-write clusters (poselets) so that the negatives have
 % poselets too (obviously those will be "null poselets" in a sense, but
 % poselets all the same).
+clusterWriteStart = tic;
 fprintf('Re-writing cluster information\n');
 biposelets = cluster_h5s(conf.biposelet_classes, conf.subposes, ...
     train_patch_dir, val_patch_dir, conf.cache_dir);
+fprintf('[T] Re-rewriting clusters took %fs\n', toc(clusterWriteStart));
 
+meanPixelStart = tic;
 fprintf('Calculating mean pixel\n');
 store_mean_pixel(train_patch_dir, conf.cache_dir);
+fprintf('[T] Writing mean pixe; took %fs\n', toc(meanPixelStart));
 
+cnnTrainStart = tic;
 fprintf('Training CNN\n');
 train_h5s = files_with_extension(train_patch_dir, '.h5');
 val_h5s = files_with_extension(val_patch_dir, '.h5');
 cnn_train(conf.cnn, conf.cache_dir, train_h5s, val_h5s);
+fprintf('[T] Training CNN took %fs\n', toc(cnnTrainStart));
 
+getPairwiseMeanStart = tic;
 fprintf('Computing ideal poselet displacements\n');
 subpose_disps = save_centroid_pairwise_means(...
     conf.cache_dir, conf.subpose_pa, conf.shared_parts);
+fprintf('[T] Computing ideal displacements took %fs\n', toc(getPairwiseMeanStart));
 
+flowCacheStart = tic;
 fprintf('Caching flow for positive validation pairs\n');
 cache_all_flow(val_dataset, conf.cache_dir);
 fprintf('Caching flow for negative pairs\n');
 cache_all_flow(neg_dataset, conf.cache_dir);
+fprintf('[T] Caching all flow took %fs\n', toc(flowCacheStart));
 
+trainGMStart = tic;
 fprintf('Training graphical model\n');
 ssvm_model = train_model(conf, val_dataset, neg_dataset, subpose_disps);
+fprintf('[T] Training graphical model took %fs\n', toc(trainGMStart));
 
-fprintf('Running bipose detections on validation set\n');
+pairDetStart = tic;
+fprintf('Running bipose detections on test set\n');
 pair_dets = get_pair_dets(conf.cache_dir, test_seqs, ssvm_model, ...
     biposelets, conf.subposes, conf.num_joints, conf.num_dets);
+fprintf('[T] Getting test bipose detections took %fs\n', toc(pairDetStart));
 
+stitchStart = tic;
 fprintf('Stitching detections into sequence\n');
 pose_dets = stitch_all_seqs(pair_dets, conf.num_stitch_dets, ...
     conf.stitch_weights, conf.valid_parts, conf.cache_dir);
 pose_gts = get_gts(test_seqs);
+fprintf('[T] Stitching validation sequences took %fs\n', toc(stitchStart));
 
 fprintf('Calculating statistics\n');
 flat_dets = cat(2, pose_dets{:});
--- main_h36m_upper.m
+++ main_h36m_upper.m
@@ -2,10 +2,23 @@
 
 startup;
 conf = get_conf_h36m_upper;
+
+% Set up the diary
+diary_dir = 'diaries';
+mkdir_p(diary_dir);
+diary_out = fullfile(diary_dir, datestr(datetime, 'yyyy-mm-ddTHH:MM:SSZ'));
+diary(diary_out);
+echo on; % Ensures that commands in this file are written to diary
+fprintf('Logging to %s\n', diary_out);
+
+% Now do what we actually wanted to
+getDataStart = tic();
 [train_dataset, val_dataset, test_seqs] = get_h36m(...
     conf.dataset_dir, conf.cache_dir, conf.subposes, conf.cnn.step, ...
     conf.template_scale, conf.trans_spec, conf.h36m_keep_frac, ...
     conf.h36m_test);
+fprintf('Getting data set took %fs\n', toc(getDataStart));
 fprintf('Training set has %i pairs, validation set has %i pairs\n', ...
     train_dataset.num_pairs, val_dataset.num_pairs);
+
 main_generic(conf, train_dataset, val_dataset, test_seqs);
